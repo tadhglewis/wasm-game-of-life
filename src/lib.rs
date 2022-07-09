cd068b737b0496b60b0b135bc34251329ba95ef2
Tadhg Lewis <tadhg.lewis@outlook.com>
2022-07-09 13:09:39 +1000

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
 mod utils;
-
+use std::fmt;
+use utils::set_panic_hook;
 use wasm_bindgen::prelude::*;
+extern crate js_sys;
+
+extern crate web_sys;
+
+// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -15,8 +26,6 @@ pub enum Cell {
     Dead = 0,
     Alive = 1,
 }
-
-use std::fmt;
 
 impl fmt::Display for Universe {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -40,6 +49,17 @@ pub struct Universe {
 }
 
 impl Universe {
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let i = self.get_index(row, col);
+            self.cells[i] = Cell::Alive
+        }
+    }
+
     fn get_index(&self, row: u32, column: u32) -> usize {
         (row * self.width + column) as usize
     }
@@ -64,6 +84,28 @@ impl Universe {
 
 #[wasm_bindgen]
 impl Universe {
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_| Cell::Dead).collect();
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_| Cell::Dead).collect();
+    }
+
     pub fn tick(&mut self) {
         let mut next = self.cells.clone();
 
@@ -90,6 +132,8 @@ impl Universe {
                     (otherwise, _) => otherwise,
                 };
 
+                // log!("it becomes {:?}", next_cell);
+
                 next[idx] = next_cell;
             }
         }
@@ -98,15 +142,17 @@ impl Universe {
     }
 
     pub fn new() -> Universe {
-        let width = 64;
-        let height = 64;
+        set_panic_hook();
+
+        let width = 128;
+        let height = 128;
 
         let cells = (0..width * height)
-            .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
-                    Cell::Alive
+            .map(|_| {
+                if js_sys::Math::random() < 0.5 {
+                    return Cell::Alive;
                 } else {
-                    Cell::Dead
+                    return Cell::Dead;
                 }
             })
             .collect();
